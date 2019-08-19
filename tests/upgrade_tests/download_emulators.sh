ed95dab2cd9b99d4eced7ef9164e45933fb33564
Tomas Susanka <tsusanka@gmail.com>
2019-08-19 13:58:49 +0200

    x

--- tests/upgrade_tests/download_emulators.sh
+++ tests/upgrade_tests/download_emulators.sh
@@ -3,7 +3,7 @@ SITE="https://firmware.corp.sldev.cz/upgrade_tests/"
 cd "$(dirname "$0")"
 
 # download all emulators without index files, without directories and only if not present
-wget --no-clobber --no-parent --cut-dirs=2 --no-host-directories --recursive --reject "index.html*" -P emulators/ https://firmware.corp.sldev.cz/upgrade_tests/
+wget --quiet --no-clobber --no-parent --cut-dirs=2 --no-host-directories --recursive --reject "index.html*" -P emulators/ https://firmware.corp.sldev.cz/upgrade_tests/
 
 # TODO: is this a good idea?
 chmod u+x emulators/trezor-emu-*
